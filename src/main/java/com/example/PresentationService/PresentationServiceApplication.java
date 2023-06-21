516ac3a31e2f1cb54cb30ed8d46063a7a06fb8e1
Jabez44 <yacobhareg@gmail.com>
2023-06-20 22:04:49 -0500

    Presentation Service - updating the csv printer

---
+++ src/main/java/com/example/PresentationService/PresentationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.PresentationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class PresentationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentationServiceApplication.class, args);
+	}
+
+}
